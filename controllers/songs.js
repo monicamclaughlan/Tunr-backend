842b8e551d67f43a9e54497e25934e78f3a59766
youngjerald239 <youngjerald239@gmail.com>
2021-05-20 11:01:36 -0500

    added the songSchema

--- controllers/songs.js
+++ controllers/songs.js
@@ -3,10 +3,26 @@ const router = require("express").Router();
 
 const { Router } = require("express");
 //IMPORT OUR MODEL
-
+const Song = require("../models/song")
 
 // SEED DATA FOR SEED ROUTE
-
+const songSeed = [
+    {
+      title: "Yummy in my tummy",
+      artist: "Yummy and the Yum Yums",
+      time: 3,
+    },
+    {
+      name: "Breakfast Kingdom",
+      img: "https://static.wikia.nocookie.net/lego-dimensions/images/4/45/BREAKFASTKINGDOMSECRET.JPG/revision/latest/scale-to-width-down/2000?cb=20161120194331",
+      description: "The Breakfast Kingdom is a state located in the Desert Lands. It is ruled by Breakfast Princess, Toast Princess, and Strudel Princess and is inhabited by Breakfast People.",
+    },
+    {
+      name: "Earldom of Lemongrab",
+      img: "https://cdn.wallpapersafari.com/85/1/U5Qtek.png",
+      description: "Earldom of Lemongrab (better known as Lemongrab) is an earldom currently ruled by the third Earl of Lemongrab. It is located in the Land of Ooo and is a subordinate territory of the Candy Kingdom.",
+    },
+  ];
 
 // ROUTES (async, since database actions are asynchronous)
 
--- models/song.js
+++ models/song.js
@@ -5,6 +5,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
-
+// Create Song Schema
+const songSchema = new Schema(
+    {
+      title: String,
+      artist: String,
+      time: Number,
+    },
+    { timestamps: true }
+  );
+  
+  // Create our Model Object
+  const Song = model("Song", songSchema);
 
 module.exports = Song
