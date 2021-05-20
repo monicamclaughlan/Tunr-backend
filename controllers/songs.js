2a0257d9207feb4168b396c1b53ff9661f7acdd5
Monica McLaughlan <monicamclaughlan@MacBook-Air.Home>
2021-05-19 21:14:31 -0400

    setup intial files for backend

---
+++ controllers/songs.js
@@ -0,0 +1,31 @@
+// CREATE A NEW EXPRESS ROUTE
+const router = require("express").Router();
+
+const { Router } = require("express");
+//IMPORT OUR MODEL
+
+
+// SEED DATA FOR SEED ROUTE
+
+
+// ROUTES (async, since database actions are asynchronous)
+
+// Seed Route for Seeding Database
+
+
+//INDEX ROUTE - GET
+
+
+// CREATE ROUTE - POST
+
+
+
+// UPDATE ROUTE - PUT - FAVORITES
+
+
+// DELETE ROUTE
+
+
+
+// export the router which has all our routes registered to it
+module.exports = router;
---
+++ models/song.js
@@ -0,0 +1,10 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+
+// Pull Schema and model from mongoose
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+
+
+module.exports = Song
