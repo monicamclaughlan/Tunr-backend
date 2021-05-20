ec5653ba4d437c5bd592bdc0b7134d19b606c5b0
youngjerald239 <youngjerald239@gmail.com>
2021-05-20 11:31:44 -0500

    switched to dev branch

--- models/song.js
+++ models/song.js
@@ -5,6 +5,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
-
+// Create Song Schema
+const songSchema = new Schema(
+    {
+      title: String,
+      artist: String,
+      time: String,
+      favorite: {type: Boolean, default: false}
+    },
+    
+    { timestamps: true }
+  );
+  
+  // Create our Model Object
+  const Song = model("Song", songSchema);
 
 module.exports = Song
